04fc7ab3cb54dc1f5c4166b6ea07228fac3f71ea
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-10-06 16:29:14 +0900

    04

--- main.c
+++ main.c
@@ -5,23 +5,18 @@
 
 int main(int argc, char *argv[]) {
 	
-	int num=0;
-	char c;
-	
-	printf("input a string:");
+	int input;
+	int i;
+	int sum=0;
 
-	while ( (c=getchar()) != '\n' ) // c에 문자 하나 하나 넣음. 문자열이 끝날때 까지 반복 
-	// getchar가 scanf역할
-	// (c = getchar() )가  '\n'와 다르면 반복 
-	 {
-	 	if(c >= '0' && c <= '9')
-	 	{
-	 		num++; // num이 1씩 증가 
-		 }
-	 }
-	 
-		
-	printf("the number of digits is %i\n", num);
+
+	printf("input a number:");
+	scanf("%i", &input);
+
+	for (i=1; i<=input; i++) // 초기식, 조건식, 증감식
+		sum += i;
+
+	printf("The result is %i\n", sum);
 	
 	return 0;
 }
