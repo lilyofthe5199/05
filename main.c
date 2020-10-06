b14a1a94e43df7d90fbaa4810d3e6d5745c33dee
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-10-06 16:12:59 +0900

    03

--- main.c
+++ main.c
@@ -5,15 +5,23 @@
 
 int main(int argc, char *argv[]) {
 	
-	int input;
+	int num=0;
+	char c;
+	
+	printf("input a string:");
 
-	printf("정수 하나를 입력하시오 :");
-	scanf("%i",&input);
-
-	if (input < 0)
-		input = input*(-1);
-
-	printf("절대값은 %i 입니다.\n", input);
+	while ( (c=getchar()) != '\n' ) // c에 문자 하나 하나 넣음. 문자열이 끝날때 까지 반복 
+	// getchar가 scanf역할
+	// (c = getchar() )가  '\n'와 다르면 반복 
+	 {
+	 	if(c >= '0' && c <= '9')
+	 	{
+	 		num++; // num이 1씩 증가 
+		 }
+	 }
+	 
+		
+	printf("the number of digits is %i\n", num);
 	
 	return 0;
 }
